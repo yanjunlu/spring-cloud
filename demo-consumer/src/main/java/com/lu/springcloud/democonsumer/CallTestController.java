0a9d17f16299f72b0650c71b5d73654ea4a2f047
yanjunlu <yanjunlu1980@gmail.com>
2018-08-12 23:53:09 +0800

    add zipkin and sleuth

--- demo-consumer/src/main/java/com/lu/springcloud/democonsumer/CallTestController.java
+++ demo-consumer/src/main/java/com/lu/springcloud/democonsumer/CallTestController.java
@@ -1,9 +1,13 @@
 package com.lu.springcloud.democonsumer;
 
+import brave.Tracer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 /**
  * @author luyanjun
@@ -11,12 +15,18 @@ import org.springframework.web.client.RestTemplate;
  **/
 @RestController
 public class CallTestController {
+    private static Logger logger = LoggerFactory.getLogger(CallTestController.class);
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private Tracer tracer;
 
     @RequestMapping("/callhello")
     public String callHello() {
         String result = restTemplate.getForObject("http://demo-provider/hello", String.class);
+        logger.info("traceId:{}", tracer.currentSpan().context().traceIdString());
+        logger.info("traceId/spanId:{}", tracer.currentSpan().toString());
         return result;
     }
 }
